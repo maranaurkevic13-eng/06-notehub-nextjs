0e6028cf283298af55cc4f551fe35aad6f2b091c
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-05-26 08:45:27 +0300

    copleted the task

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,34 +1,28 @@
-import css from './Pagination.module.css';
-import type { ReactPaginateProps } from 'react-paginate';
-import ReactPaginateModule from 'react-paginate';
-import type { ComponentType } from "react";
+import ReactPaginate from "react-paginate";
+import css from "./Pagination.module.css";
 
 interface PaginationProps {
-  pageCount: number,
-  currentPage: number,
-  onPageChange: (page: number) => void
-}
+  pageCount: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}           
 
-type ModuleWithDefault<T> = { default: T };
-const ReactPaginate = (
-  ReactPaginateModule as unknown as ModuleWithDefault<
-    ComponentType<ReactPaginateProps>
-  >
-).default;
-
-export default function Pagination({pageCount,currentPage, onPageChange}: PaginationProps) {
-    return (
-        <ReactPaginate
+export default function Pagination({
+  pageCount,
+  currentPage,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <ReactPaginate
       pageCount={pageCount}
       pageRangeDisplayed={5}
       marginPagesDisplayed={1}
-        forcePage={currentPage}
-        
-     onPageChange={(event: { selected: number }) => onPageChange(event.selected)}
+      forcePage={currentPage}
+      onPageChange={(event) => onPageChange(event.selected)}
       containerClassName={css.pagination}
       activeClassName={css.active}
       nextLabel="→"
       previousLabel="←"
     />
-    )
+  );
 }
